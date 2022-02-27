f843ccdf0241f517c65e8a0872ef7a637ee74aeb
AbiyyuSyabirin <89232989+AbiyyuSyabirin@users.noreply.github.com>
2022-02-27 17:59:42 +0700

    Add files via upload

---
+++ TestClass5026211162.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class TestClass5026211162{
+
+	public static void main(String[] args){
+	
+	ComputeMethods5026211162 compMeth = new ComputeMethods5026211162();
+
+	compMeth.fToC(100);
+
+	compMeth.hypotenuse(7,24);
+	
+	compMeth.roll();
+
+	}
+}
